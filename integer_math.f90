a52f92004738fec9f0da84732ddbc3e8016dbd54
Richard Forth <richard.forth@gmail.com>
2023-11-28 09:09:19 +0000

    Update integer_math.f90
    oops

--- integer_math.f90
+++ integer_math.f90
@@ -1,1 +1,15 @@
-i
+program integer_math
+
+    integer :: i = 1
+    integer :: result
+
+    result = i  / 2
+
+    ! note that in fortran integer math dictates that any fractional elements are truncated
+    ! it is not rounded up or down like in normal maths, hence:
+    ! 1 / 2
+    ! 0.5
+    ! 0  
+    print *, result
+
+end program integer_math
