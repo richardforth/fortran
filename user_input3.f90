d7ad70bd0717f1692b90069c80cfc7dd739312cf
Richard A. Forth <richard.forth@gmail.ccom>
2023-11-29 20:01:08 +0000

    Added program that takes additional inputs with default values

---
+++ user_input3.f90
@@ -0,0 +1,19 @@
+program user_input
+    implicit none
+
+    character(len=20) :: name
+    integer :: age
+    character(len=20) :: hometown
+
+
+    name = "UNKNOWN"
+    age = 0
+    hometown = "UNKNOWN"
+
+    print *, "What is your name, age and hometown?"
+    read(*, *) name, age, hometown
+    
+    print *, "Hello, " // trim(name) // "!"
+    print *, "You don't look old enough to be ", age, "!"
+    print *, "What's the weather like in " // trim(hometown) // "?"
+end program user_input
