c39f981e1af8ca5b8c4bdb41cadd27932d252eb5
Richard A. Forth <richard.forth@gmail.ccom>
2023-11-29 22:49:33 +0000

    Updated

--- sillyprogs/bill_totter.f90
+++ sillyprogs/bill_totter.f90
@@ -1,4 +1,14 @@
 program bill_totter
+    ! Recommended execution: ./bill_totter | column -t
+    !
+    ! bill.txt should have two items per line:
+    ! item, real number, eg:
+    ! AVOID SPACES IN ITEM 
+    !coffee 1.25
+    !soup 4.95
+    !toast 2.00
+    !ice-cream 4.95
+
     implicit none
 
     real :: total
@@ -10,6 +20,8 @@ program bill_totter
 
     open(newunit=unit, file="bill.txt", status="OLD")
     
+    print *, "Description Item Subtotal"
+    print *, "----------- ---- --------"   
     ! read until end of file
     do 
         read(unit, *, iostat=status) item, amount
@@ -17,7 +29,7 @@ program bill_totter
         total = total + amount
         print '(a, F0.2, a, F0.2)', item, amount," ", total
     end do
-    
-    print '(a, F0.2)',  "Total Amount on Bill: ", total 
+    print *, "____________"   
+    print '(a, F0.2)',  "Total ____ ", total 
     close(unit) 
 end program bill_totter
